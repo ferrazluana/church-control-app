1c0e7921b7020e13c333b63b7ad0c905e02a5e86
Luana Ferraz <luanarufino94@gmail.com>
2025-02-05 23:10:16 -0300

    vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Garante que os caminhos das rotas estão corretos
   resolve: {
     extensions: ['.js', '.jsx']
   }
